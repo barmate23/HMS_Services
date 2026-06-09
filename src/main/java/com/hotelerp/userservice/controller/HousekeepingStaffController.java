2c6b4a1d1d9604ced25770106e92b98ac4929863
atharv0809 <atharv.deshpande@sarvosmi,io>
2026-06-09 11:38:29 +0530

    add the API for the house keeping assign the rooms

--- src/main/java/com/hotelerp/userservice/controller/HousekeepingStaffController.java
+++ src/main/java/com/hotelerp/userservice/controller/HousekeepingStaffController.java
@@ -2,8 +2,10 @@ package com.hotelerp.userservice.controller;
 
 import com.hotelerp.userservice.common.StandardResponse;
 import com.hotelerp.userservice.dto.StaffDTO;
+import com.hotelerp.userservice.dto.UserRoomAssignmentRequest;
 import com.hotelerp.userservice.service.StaffService;
 import com.hotelerp.userservice.constant.ServiceConstant;
+import com.hotelerp.userservice.service.UserRoomMapService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +18,13 @@ import java.util.List;
 public class HousekeepingStaffController {
 
     private final StaffService staffService;
+    private final UserRoomMapService userRoomMapService;
+
+
+    @PostMapping("/saveAssignments")
+    public StandardResponse<Void> saveAssignments(@RequestBody UserRoomAssignmentRequest request) {
+        return userRoomMapService.syncUserRooms(request);
+    }
 
     @GetMapping(ServiceConstant.GET_HOUSEKEEPING_STAFF)
     public ResponseEntity<StandardResponse<List<StaffDTO>>> getHousekeepingStaff() {
